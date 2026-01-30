8329ec85c306689e29b49b983731cd6978c0bf18
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2026-01-30 19:18:39 -0300

    Update info-futabucasino.js

--- commands/group/info-futabucasino.js
+++ commands/group/info-futabucasino.js
@@ -1,55 +1,37 @@
-let handler = async (m, { conn, command }) => {
+export default {
+  command: [
+    'reglasfutabucasino', 'rulesfutabucasino', 'reglasfc',
+    'rolcasino', 'casinoinfo', 'infoc',
+    'rolgacha', 'gachainfo', 'infog'
+  ],
+  category: 'grupo',
+  
+  run: async (client, m, args, usedPrefix, command, text) => {
     
-    if (/^(reglasfutabucasino|rulesfutabucasino|reglasfc)$/i.test(command)) {
-        let texto = `╰Futabu Casino╯
-
-📝| Reglas:
-❖ Futabu Club
-➥ Es *OBLIGATORIO* que estes a la vez en grupo de Futabu Club y que tambien aportes o hables por ahi.
-❖ Respeto
-➥ Se debe respetar a todos en el grupo.
-❖ Peleas o Discusiones
-➥ Resolver en privado.
-❖ Flood
-➥ Prohibido inundar el chat.`
-        return m.reply(texto)
+    // REGLAS CASINO
+    if (['reglasfutabucasino', 'rulesfutabucasino', 'reglasfc'].includes(command)) {
+       return m.reply(`╰Futabu Casino╯\n\n📝| Reglas:\n❖ Futabu Club: Es *OBLIGATORIO* estar en el grupo principal.\n❖ Respeto, Cero Toxicidad y No Flood.`);
     }
 
-    if (/^(rolcasino|casinoinfo|infoc)$/i.test(command)) {
-        let texto = `🎰|Comandos Casino/RPG/Economia:
+    // INFO ROL CASINO
+    if (['rolcasino', 'casinoinfo', 'infoc'].includes(command)) {
+       return m.reply(`🎰|Comandos Casino/RPG/Economia:
 
 _*[BOT MITAMA]*_
-• #bal - Ver tu balance.
-• #work - Gana Futacoins trabajando.
-• #slut - Gana Futacoins prostituyéndote.
-• #crime - Gana Futacoins haciendo un crimen.
-• #dep - Depositar tus Futacoins.
-• #with - Retirar tus Futacoins.
-• #pay - Darle Futacoins a un usuario.
-
+• #bal, #work, #slut, #crime, #dep, #with, #pay
 _*[BOT ASAKURA]*_
-• .adventure - Comenzar un adventura.
-• .cazar - Caza algún objeto.
-• .balance - Ver tu balance.
-• .minar - Mina para obtener diamantes.`
-        return m.reply(texto)
+• .adventure, .cazar, .cofre, .balance, .minar`);
     }
 
-    if (/^(rolgacha|gachainfo|infog)$/i.test(command)) {
-        let texto = `🌸| Comandos Gacha:
+    // INFO GACHA
+    if (['rolgacha', 'gachainfo', 'infog'].includes(command)) {
+       return m.reply(`🌸| Comandos Gacha:
 
 _*[BOT MITAMA]*_
 • #rw - Girar waifu.
 • #waifus - Ver tus waifus.
 • #c - Reclamar waifu.
-• #trade - Intercambiar waifu.
-• #sell - Vender waifu.`
-        return m.reply(texto)
+• #trade - Intercambiar waifu.`);
     }
+  }
 }
-
-handler.help = ['reglasfutabucasino', 'rolcasino', 'rolgacha']
-handler.tags = ['grupo']
-handler.command = /^(reglasfutabucasino|rulesfutabucasino|reglafutabucasino|rulefutabucasino|reglasfc|rolcasino|casinoinfo|infoc|rolgacha|gachainfo|infog)$/i
-handler.group = true
-export default handler
